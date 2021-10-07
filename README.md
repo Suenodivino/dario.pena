decf32ab3ea86e708426dfd5e000b0150ff246ac
suenodivino <depena210@gmail.com>
2021-10-06 21:10:19 -0400

    added text

--- README.md
+++ README.md
@@ -10,4 +10,4 @@
 
 # The tip calculator will ask for the number of persons splitting the bill.
 
-# The tip calculator will then output the payment, including the tip, and a 10% sales tax, for the number of people in equal amount.
+# The tip calculator will then output the bill, including the tip, and a 10% sales tax, for the number of people in equal amount.
--- tip_calculator.py
+++ tip_calculator.py
@@ -15,14 +15,18 @@ print()
 
 # The tip calculator asks the customer to input the total bill amount.
 
-bill = float(input('Enter the total bill: '))
+bill = float(input('Enter total bill: '))
 
 # The variable "tip" is assigned a string that will output or print "Enter tip %" 
 # to the terminal and convert the customer input the tip percentage amount to the integer number format. 
 
 # The tip calculator will ask the customer to input the tip percentage amount.
 
-tip = int(input('Enter tip %: '))
+tip = int(input('Enter tip%: '))
+
+# The variable "sales_tax" is assigned 10% tax for the total cost of the meal.
+
+tax = (.10 * bill)
 
 # The variable "split_bill" is assigned a string that will output or print "How many people are splitting the bill"
 # to the terminal and accept the customer input number of people splitting the bill as an integer.
@@ -31,10 +35,11 @@ tip = int(input('Enter tip %: '))
 
 split_bill = int(input('How many people splitting the bill?: '))
 
-# The variable "payments" will output the amount that each person will pay, based on the tip percentage,
-# multiplied by the total bill, and divided by the total number of people entered by the customer.
+# The variable "payments" will output the amount that each person will pay, based on the tip percentage (which is 
+# divided by a hundred), multiplied by the total bill, and divided by the total number of people entered by the customer.
 
 # The tip calculator will then calculate the amount each person will pay, including tip amount.
 
-payments = (round(float(((tip/100 + 1)* bill)/split_bill), 2))
-print(f'Payment for each: ${payments}')
+payments = (float(tip/100 + 1)* bill)/split_bill + tax/split_bill
+payments = round(payments, 2)
+print(f'payment for each: ${payments}')
