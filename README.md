c8c70547b772935d9bc1d234e38814b22555f8c7
suenodivino <depena210@gmail.com>
2021-10-10 01:59:26 -0400

    Added completed tip calculator and read me files to my repo

--- README.md
+++ README.md
@@ -4,14 +4,16 @@
 
 # The tip calculator will output on the second line a well wish to the customer for the enjoyment of his/her meal.
 
-# The tip calculator will ask on the third line for the total amount of the bill.
+# The tip calculator will ask on the third line for the total bill of the food.
 
 # The tip calculator will ask on the fourth line for the tip percentage amount.
 
 # The tip calculator will ask on the fifth line for the number of persons splitting the bill.
 
-# The tip calculator will then calculate the bill, dividing the tip percentage amount as a float by 100, multiplying the resulting number by the bill, adding a 10% sales tax to the resulting number, and dividing the resulting number by the number of people paying in equal amount.
+# The tip calculator will then calculate the bill, divide the tip percentage amount as a float by 100, adding the resulting number to the bill, adding a 10% sales tax to the resulting number.
+
+# The tip calculator will output on the sixth line the total amount of the bill by dividing the total bill by the split bill as a float, or decimal number.
 
 # The tip calculator will then round the resulting number that each person will pay with two decimal numbers behind the decimal point.
 
-# The tip calculator will output the equal amount that each person will pay to the terminal.
+# The tip calculator will output the equal amount that each person will pay to the terminal as a float, or decimal number.
--- tip_calculator.py
+++ tip_calculator.py
@@ -9,12 +9,12 @@ print('Thank you for using the Biltipulator')
 print('Hope you enjoyed your meal!')
 print()
 
-# The variable "bill" is assigned a string that will output or print "Enter the total bill" to the terminal
-# and will convert the customer input of the total bill amount to the form of a float or decimal number.
+# The variable "bill" is assigned a string that will output or print "Enter the total bill for food" to the 
+# terminal and will convert the customer input of the total bill amount to the form of a float or decimal number.
 
 # The tip calculator asks the customer to input the total bill amount.
 
-bill = float(input('Enter total bill: '))
+bill = float(input('Enter total bill for food: '))
 
 # The variable "tip" is assigned a string that will output or print "Enter tip %" to the terminal and
 # the customer input amount will be stored in integer number format. 
@@ -23,10 +23,17 @@ bill = float(input('Enter total bill: '))
 
 tip = int(input('Enter tip%: '))
 
-# The variable "tax" is assigned 10% of the total bill as a float to be added to the total cost of 
-# the meal.
+# The variable "tax" is assigned 10% of the total bill as a float to be added to the total cost of the meal.
 
 tax = (.10 * bill)
+
+# The tip calculator will calculate the total bill by adding the bill to the the tip amount and the sales tax.
+
+total_bill = float((bill + tip/100) + tax)
+
+# The tip calculator will add two decimal places behind the float, or total bill number.
+
+total_bill = round(total_bill, 2)
 
 # The variable "split_bill" is assigned a string that will output or print "How many people are splitting the 
 # bill?" to the terminal, and the customer input number of people splitting the bill as an integer.
@@ -39,16 +46,19 @@ split_bill = int(input('How many people splitting the bill?: '))
 # to get the amount for the tip, multiplied by the total bill, which resulting number is then divided by the
 # number of people paying, and the 10% sales tax is added to the result.
 
-# The variable "payments" will calculate the amount that each person will pay, based on the tip percentage 
-# (which is divided by a hundred), multiplied by the total bill, and divided by the total number of people 
-# entered by the customer, adding the 10% sales tax and dividing this by the number of people paying.
+# The variable "payments" will calculate the amount that each person will pay by dividing the total bill by
+# the split bill, or the amount of people paying. 
 
-payments = (float(tip/100 + 1)* bill)/split_bill + tax/split_bill
+payments = (float(total_bill/split_bill))
 
 # The variable "payments" will only add two numbers behind the decimal number, which will be a float.
 
 payments = round(payments, 2)
 
+# The tip calculator will output the total bill, including the tip and sales tax.
+
+print(f'Total bill amount: ${total_bill}')  
+
 # The print function will now output to the terminal the amount that each person will pay.
-  
+
 print(f'payment for each: ${payments}')
